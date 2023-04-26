695bc0996eeff1b6b3c86a572859c328a5f4559c
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-26 22:28:14 +0300

    feat: add create project button

---
+++ client/src/components/createProject/CreateProject.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./styles.module.css";
+
+
+const CreateProject = () => {
+  return (
+    <button className={styles.wrapper}>
+      Создать новый проект
+    </button>
+  )
+}
+
+export default CreateProject
