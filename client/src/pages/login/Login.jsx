f0daef387260bb5d517537b356379cc48fde845b
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-26 14:16:23 +0300

    feat: add error message to the login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,16 +1,32 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./styles.module.css";
 import Link from "../../components/link/Link";
 import Button from "../../components/button/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../store/reducers/authReducer/actions";
+import ErrorMessage from "../../components/errorMessage/ErrorMessage";
 
 const Login = () => {
   const username = useRef();
   const password = useRef();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessageActive, setErrorMessageActive] = useState(false);
 
   const dispatch = useDispatch();
   const { isFetching, error } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    console.log(error);
+    if (error) {
+      setErrorMessage(error?.response.data.message);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (errorMessage !== "") {
+      setErrorMessageActive(true);
+    }
+  }, [errorMessage]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -48,6 +64,13 @@ const Login = () => {
           </div>
         </form>
       </div>
+
+      <ErrorMessage
+        active={errorMessageActive}
+        setActive={setErrorMessageActive}
+      >
+        {errorMessage}
+      </ErrorMessage>
     </div>
   );
 };
