cf4d4375543d0545bf1d50a9deecfe77262a3bf1
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-26 12:55:09 +0300

    feat: add modal error message

---
+++ client/src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const ErrorMessage = ({ active, setActive, children }) => {
+  return (
+    <div
+      className={active ? `${styles.window} ${styles.active}` : styles.window}
+      onClick={() => setActive(false)}
+    >
+      <div
+        className={
+          active ? `${styles.content} ${styles.active}` : styles.content
+        }
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ErrorMessage;
