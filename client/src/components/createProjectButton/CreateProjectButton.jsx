37398a126023c6fc34df3b495de6d47d60a03df4
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-27 17:10:42 +0300

    feat: add redirect to the project creation page

--- client/src/components/createProjectButton/CreateProjectButton.jsx
+++ client/src/components/createProjectButton/CreateProjectButton.jsx
@@ -1,13 +1,18 @@
-
-import React from 'react'
+import React from "react";
 import styles from "./styles.module.css";
+import { useNavigate } from "react-router-dom";
 
 const CreateProjectButton = () => {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/createProject");
+  }
   return (
-    <button className={styles.wrapper}>
+    <button className={styles.wrapper} onClick={handleClick}>
       Создать новый проект
     </button>
-  )
-}
+  );
+};
 
-export default CreateProjectButton
+export default CreateProjectButton;
