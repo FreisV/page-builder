09d3eeceee66c37786c4acc1a9a522a2afa87713
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-27 15:30:10 +0300

    fear: add routes for projects

--- server/routers/index.js
+++ server/routers/index.js
@@ -3,7 +3,9 @@ const userController = require("../controllers/user-controller");
 const router = new Router();
 const { body } = require("express-validator");
 const authMiddleware = require("../middlewares/auth-middleware");
+const projectController = require("../controllers/project-controller");
 
+//authorization
 router.post(
   "/registration",
   body("username").isLength({ min: 3, max: 32 }),
@@ -16,5 +18,12 @@ router.post("/logout", userController.logout);
 router.get("/activate/:link", userController.activate);
 router.get("/refresh", userController.refresh);
 router.get("/users", authMiddleware, userController.getUsers);
+
+//projects
+router.post("/projects", authMiddleware, projectController.create);
+router.put("/projects/:id", authMiddleware, projectController.update);
+router.get("/projects/:id", authMiddleware, projectController.get);
+router.get("/userProjects", authMiddleware, projectController.getUserProjects);
+router.delete("/projects/:id", authMiddleware, projectController.delete);
 
 module.exports = router;
