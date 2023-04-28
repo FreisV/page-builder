519f91820436856173f6369ebbcce2f3bc1d4480
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-28 21:12:34 +0300

    feat: add getting user projects

--- client/src/services/ProjectService.js
+++ client/src/services/ProjectService.js
@@ -4,4 +4,8 @@ export default class ProjectService {
   static async create(name, isOpen) {
     return api.post("/projects", {name, isOpen})
   }
+
+  static async getUserProjects() {
+    return api.get("/userProjects");
+  }
 }
--- client/src/store/reducers/requestReducer/actions/projectActions.js
+++ client/src/store/reducers/requestReducer/actions/projectActions.js
@@ -4,3 +4,7 @@ import { createRequestAction } from "../actions";
 export const createProject = (name, isOpen) => {
   return createRequestAction(() => ProjectService.create(name, isOpen));
 };
+
+export const getUserProjects = () => {
+  return createRequestAction(() => ProjectService.getUserProjects());
+}
