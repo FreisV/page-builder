47b3dc7de943ea5afd62abc5c907bcdab6bc4aa6
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-28 20:50:22 +0300

    fix: fix uncorrect hover effect

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 import styles from "./styles.module.css";
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const { username } = useSelector((state) => state?.auth?.user);
 
-
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
-        <Link className={styles.logo} to={"/projects"}>Builder</Link>
+        <Link className={styles.logo} to={"/projects"}>
+          Builder
+        </Link>
         <div className={styles.profile}>
           <p>{username}</p>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
