2fa069dada904c41f3c5cbce07fbcf23b94bef3b
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-28 19:20:37 +0300

    feat: add interceptor updating tokens or exiting the account

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { logout } from "../store/reducers/authReducer/actions";
+import store from '../store';
 
 export const API_URL = "http://localhost:5000/api";
 
@@ -11,5 +13,23 @@ api.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${localStorage.getItem("accessToken")}`;
   return config;
 });
+
+api.interceptors.response.use((config) => {
+  return config;
+}, async (error) => {
+  const originalRequest = error.config;
+  if (error.response.status === 401 && error.config && !error.config._isRetry) {
+      originalRequest._isRetry = true;
+      try {
+          const response = await axios.get(`${API_URL}/refresh`, {withCredentials: true})
+          localStorage.setItem('accessToken', response.data.accessToken);
+          return api.request(originalRequest);
+      } catch (e) {
+        const { dispatch } = store;
+        dispatch(logout());
+      }
+  }
+  throw error;
+})
 
 export default api;
