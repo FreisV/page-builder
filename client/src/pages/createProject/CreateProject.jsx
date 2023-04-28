0948d2c17660137f0d1169d9d760a86cdd6f4ec3
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-28 18:23:00 +0300

    feat: add a project creation on the create project page

--- client/src/pages/createProject/CreateProject.jsx
+++ client/src/pages/createProject/CreateProject.jsx
@@ -1,23 +1,68 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./styles.module.css";
 import Header from "../../components/header/Header";
 import Button from "../../components/button/Button";
+import { createProject } from "../../store/reducers/requestReducer/actions/projectActions";
+import { useDispatch, useSelector } from "react-redux";
+import ErrorMessage from "../../components/errorMessage/ErrorMessage";
+import { useNavigate } from "react-router-dom";
 
 const CreateProject = () => {
+  const name = useRef();
+  const isOpen = useRef();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessageActive, setErrorMessageActive] = useState(false);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.requset);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const project = await dispatch(createProject(name.current.value, isOpen.current.checked));
+    if (project) {
+      navigate("/projects");
+    }
+  };
+
+  useEffect(() => {
+    if (error) {
+      setErrorMessage(error.message);
+    }
+  }, [error])
+
+  useEffect(() => {
+    if (errorMessage !== "") {
+      setErrorMessageActive(true);
+    }
+  }, [errorMessage])
+
   return (
     <>
       <Header />
       <main className={styles.wrapper}>
         <h2 className={styles.title}>Создать новый проект</h2>
-        <form className={styles.form} action="">
-          <input className={styles.input} type="text" placeholder="Название" />
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input
+            className={styles.input}
+            ref={name}
+            type="text"
+            placeholder="Название"
+          />
           <label className={styles.label}>
-            <input className={styles.checkbox} type="checkbox" /> Доступен к
-            просмотру ?
+            <input className={styles.checkbox} ref={isOpen} type="checkbox" />{" "}
+            Доступен к просмотру ?
           </label>
           <Button type="submit">Создать</Button>
         </form>
       </main>
+      
+      <ErrorMessage
+        active={errorMessageActive}
+        setActive={setErrorMessageActive}
+      >
+        {errorMessage}
+      </ErrorMessage>
     </>
   );
 };
