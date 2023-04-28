86484d878636333ce01de572490018d2bfe17731
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-28 18:22:16 +0300

    feat: add a project creation function

---
+++ client/src/store/reducers/requestReducer/actions/projectActions.js
@@ -0,0 +1,6 @@
+import ProjectService from "../../../../services/ProjectService";
+import { createRequestAction } from "../actions";
+
+export const createProject = (name, isOpen) => {
+  return createRequestAction(() => ProjectService.create(name, isOpen));
+};
