26261dfa903b7151cd0f3707b58ad65d6e1b9356
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-29 15:46:36 +0300

    feat: add get, update and delete project functions

--- client/src/services/ProjectService.js
+++ client/src/services/ProjectService.js
@@ -2,7 +2,19 @@ import api from "../http";
 
 export default class ProjectService {
   static async create(name, isOpen) {
-    return api.post("/projects", {name, isOpen})
+    return api.post("/projects", { name, isOpen });
+  }
+
+  static async get(id) {
+    return api.get(`/projects/${id}`);
+  }
+
+  static async update(id, name, isOpen) {
+    return api.put(`/projects/${id}`, { name, isOpen });
+  }
+
+  static async delete(id) {
+    return api.delete(`/projects/${id}`);
   }
 
   static async getUserProjects() {
--- client/src/store/reducers/requestReducer/actions/projectActions.js
+++ client/src/store/reducers/requestReducer/actions/projectActions.js
@@ -5,6 +5,19 @@ export const createProject = (name, isOpen) => {
   return createRequestAction(() => ProjectService.create(name, isOpen));
 };
 
+export const getProject = (id) => {
+  return createRequestAction(() => ProjectService.get(id));
+};
+
+export const updateProject = (id, name, isOpen) => {
+  return createRequestAction(() => ProjectService.update(id, name, isOpen));
+
+}
+
+export const deleteProject = (id) => {
+  return createRequestAction(() => ProjectService.delete(id));
+};
+
 export const getUserProjects = () => {
   return createRequestAction(() => ProjectService.getUserProjects());
-}
+};
