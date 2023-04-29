3f2e180a02b2b09191fdf63752959b616947dc07
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-29 15:50:15 +0300

    feat: add route to the project updating

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Registration from "./pages/registration/Registration";
 import Projects from "./pages/projects/Projects";
 import { useSelector } from "react-redux";
 import CreateProject from "./pages/createProject/CreateProject";
+import UpdateProject from "./pages/updateProject/UpdateProject";
 
 function App() {
   const { user } = useSelector((state) => state.auth);
@@ -23,6 +24,10 @@ function App() {
         <Route
           path="/createProject"
           element={user ? <CreateProject /> : <Navigate replace to="/login" />}
+        />
+        <Route
+          path="/project/settings/:id"
+          element={user ? <UpdateProject /> : <Navigate replace to="/login" />}
         />
         <Route
           path="/login"
