79a1205fb13679c966a9f1c5d7ab1250863e1025
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-29 16:33:11 +0300

    feat: add field updatedAt to the projectShema

--- server/models/project-model.js
+++ server/models/project-model.js
@@ -4,6 +4,7 @@ const ProjectSchema = new Schema({
   owner: { type: Schema.Types.ObjectId, ref: "User" },
   name: { type: String, required: true },
   isOpen: { type: Boolean, default: false },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = model("Project", ProjectSchema);
