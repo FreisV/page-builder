5273095defac40f830166e628620433615f12700
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-30 15:25:42 +0300

    feat: add ownerId field to project-dto

--- server/dtos/project-dto.js
+++ server/dtos/project-dto.js
@@ -1,10 +1,12 @@
 module.exports = class ProjectDto {
   id;
+  ownerId;
   name;
   isOpen;
 
   constructor(model) {
     this.id = model._id;
+    this.ownerId = model.ownerId;
     this.name = model.name;
     this.isOpen = model.isOpen;
   }
