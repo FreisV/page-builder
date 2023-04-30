1012daca126b4d26577b3d6cdd988642c749d94e
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-30 14:48:49 +0300

    feat: create block controller

---
+++ server/controllers/block-controller.js
@@ -0,0 +1,60 @@
+const ApiError = require("../exceptions/api-error");
+const blockService = require("../services/block-service") 
+
+class BlockController {
+  async create(req, res, next) {
+    try {
+      const projectId = req.params.id;
+      const data = req.body;
+
+      data.projectId = projectId;
+
+      const block = await blockService.create(data);
+
+      res.json(block);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async update(req, res, next) {
+    try {
+      const projectId = req.params.id;
+      const data = req.body;
+      
+      data.projectId = projectId;
+
+      const block = await blockService.update(data);
+
+      res.json(block);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getBlocks(req, res, next) {
+    try {
+      const projectId = req.params.id;
+
+      const block = await blockService.getBlocks(projectId);
+
+      res.json(block);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getBlock(req, res, next) {
+    try {
+      const blockId = req.params.blockId;
+
+      const block = await blockService.getBlock(blockId);
+
+      res.json(block);
+    } catch (e) {
+      next(e);
+    }
+  }
+}
+
+module.exports = new BlockController();
