232d83e64ecdb54ac708679161c679e6d21fe8b9
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-04-30 14:48:25 +0300

    feat: create block dto

---
+++ server/dtos/block-dto.js
@@ -0,0 +1,14 @@
+module.exports = class BlockDto {
+  constructor(block) {
+    this.id = block._id;
+    this.blockNumber = block.blockNumber;
+    this.type = block.type;
+    if (block.type === "ButtonBlock") {
+      this.text = block.text;
+      this.link = block.link;
+      this.download = block.download;
+    } else if (block.type === "ParagraphBlock") {
+      this.text = block.text;
+    }
+  }
+}
