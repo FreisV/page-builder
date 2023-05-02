addf0a4502865c2d90e1e9fb8d1374d7cfea780a
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-03 02:39:39 +0300

    fix: fix data refresh

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { logout } from "../store/reducers/authReducer/actions";
-import store from '../store';
+import { logout, refreshData } from "../store/reducers/authReducer/actions";
+import store from "../store";
 
 export const API_URL = "http://localhost:5000/api";
 
@@ -10,26 +10,36 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${localStorage.getItem("accessToken")}`;
+  config.headers.Authorization = `Bearer ${localStorage.getItem(
+    "accessToken"
+  )}`;
   return config;
 });
 
-api.interceptors.response.use((config) => {
-  return config;
-}, async (error) => {
-  const originalRequest = error.config;
-  if (error.response.status === 401 && error.config && !error.config._isRetry) {
+api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response.status === 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
       originalRequest._isRetry = true;
       try {
-          const response = await axios.get(`${API_URL}/refresh`, {withCredentials: true})
-          localStorage.setItem('accessToken', response.data.accessToken);
-          return api.request(originalRequest);
+        const res = await axios.get(`${API_URL}/refresh`, {
+          withCredentials: true,
+        });
+        store.dispatch(refreshData(res.data));
+        return api.request(originalRequest);
       } catch (e) {
-        const { dispatch } = store;
-        dispatch(logout());
+        await store.dispatch(logout());
       }
+    }
+    throw error;
   }
-  throw error;
-})
+);
 
 export default api;
