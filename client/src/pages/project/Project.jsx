8e756b4f666107e3d7b65abd91237fc382483d8b
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-02 16:14:46 +0300

    feat: add an error message to project page

--- client/src/pages/project/Project.jsx
+++ client/src/pages/project/Project.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 import Header from "../../components/header/Header";
 import BlockList from "../../components/blockList/BlockList";
 import { useSelector } from "react-redux";
+import ErrorMessage from "../../components/errorMessage/ErrorMessage";
 
 const Project = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessageActive, setErrorMessageActive] = useState(false);
+
   const { isFetching, error } = useSelector((state) => state.blocks);
+
+  useEffect(() => {
+    if (error) {
+      setErrorMessage(error.message);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (errorMessage !== "") {
+      setErrorMessageActive(true);
+    }
+  }, [errorMessage]);
 
   return (
     <>
@@ -13,6 +29,13 @@ const Project = () => {
       <main className={styles.wrapper}>
         <BlockList />
       </main>
+
+      <ErrorMessage
+        active={errorMessageActive}
+        setActive={setErrorMessageActive}
+      >
+        {errorMessage}
+      </ErrorMessage>
     </>
   );
 };
