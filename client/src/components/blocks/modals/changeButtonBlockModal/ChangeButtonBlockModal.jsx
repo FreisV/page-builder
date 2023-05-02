8d7d4d5f98a6e65f5ebc12787d05122809f5238a
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-02 16:16:28 +0300

    fix: add a checkbox for change a download field

--- client/src/components/blocks/modals/changeButtonBlockModal/ChangeButtonBlockModal.jsx
+++ client/src/components/blocks/modals/changeButtonBlockModal/ChangeButtonBlockModal.jsx
@@ -4,11 +4,13 @@ import { useParams } from "react-router-dom";
 import { updateBlock } from "../../../../store/reducers/blocksReducer/actions";
 import Input from "../../../input/Input";
 import ChangeBlockModal from "../changeBlockModal/ChangeBlockModal";
+import Checkbox from "../../../checkbox/Checkbox";
 
 const ChangeButtonBlockModal = ({ active, setActive, id }) => {
   const params = useParams();
   const textRef = useRef();
   const linkRef = useRef();
+  const downloadRef = useRef();
 
   const dispatch = useDispatch();
   const { blocks } = useSelector((state) => state.blocks);
@@ -17,14 +19,18 @@ const ChangeButtonBlockModal = ({ active, setActive, id }) => {
   useEffect(() => {
     textRef.current.value = block.text;
     linkRef.current.value = block.link;
-  }, [active, block.link, block.text]);
+    downloadRef.current.checked = block.download;
+  }, [active, block.download, block.link, block.text]);
 
   const handleUpdate = async (e) => {
     e.preventDefault();
     try {
       const text = textRef.current.value;
       const link = linkRef.current.value;
-      await dispatch(updateBlock(params.id, { ...block, text, link }));
+      const download = downloadRef.current.checked;
+      await dispatch(
+        updateBlock(params.id, { ...block, text, link, download })
+      );
       setActive(false);
     } catch (e) {
       console.error(e);
@@ -39,6 +45,10 @@ const ChangeButtonBlockModal = ({ active, setActive, id }) => {
     >
       <Input placeholder="Текст" forwardedRef={textRef} />
       <Input placeholder="Ссылка" forwardedRef={linkRef} />
+      <Checkbox
+        forwardedRef={downloadRef}
+        desc={"Скачивать файл при нажатии?"}
+      />
     </ChangeBlockModal>
   );
 };
