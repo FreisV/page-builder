421a0be3facdaa56af8bb17ad2c4062bdf7bb648
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-02 15:24:52 +0300

    feat: add change block modal component

---
+++ client/src/components/blocks/modals/changeBlockModal/ChangeBlockModal.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Modal from "../../../modal/Modal";
+import styles from "./styles.module.css";
+
+const ChangeBlockModal = ({ active, setActive, handleUpdate, children }) => {
+  return (
+    <Modal active={active} setActive={setActive}>
+      <div className={styles.buttons}>
+        <button className={styles.button} onClick={() => setActive(false)}>
+          Закрыть
+        </button>
+        <button
+          className={`${styles.button} ${styles.save}`}
+          onClick={handleUpdate}
+        >
+          Сохранить и закрыть
+        </button>
+      </div>
+      <h2 className={styles.title}>Изменить блок</h2>
+      <div className={styles.fields}>{children}</div>
+    </Modal>
+  );
+};
+
+export default ChangeBlockModal;
