004c16383415cf06c4efa020a9cf16ea6a9f2066
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-02 15:19:37 +0300

    feat: add block reducer to root reducer

--- client/src/store/reducers/rootReducers.js
+++ client/src/store/reducers/rootReducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer/authReducer";
 import requestReducer from "./requestReducer/requestReducer";
+import blockReducer from "./blocksReducer/blocksReducer";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
   requset: requestReducer,
+  blocks: blockReducer,
 });
