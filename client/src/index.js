a5443e6ed8c2415f117cc4703e6e20ec042340d3
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-03 02:37:24 +0300

    fix: delete the Course mode.Strict, because two-component rendering interrupts the refresh of tokens and data

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,9 @@ import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <Provider store={store}>
-      <App />
+      <App/>
     </Provider>
-  </React.StrictMode>
+  //</React.StrictMode>
 );
