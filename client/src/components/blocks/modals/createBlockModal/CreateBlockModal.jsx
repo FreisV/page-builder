dec7b99740b3548988702f155503bc9a17c8196c
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-02 15:23:21 +0300

    feat: add create block modal

---
+++ client/src/components/blocks/modals/createBlockModal/CreateBlockModal.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styles from "./styles.module.css";
+import Modal from "../../../modal/Modal";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { createBlock } from "../../../../store/reducers/blocksReducer/actions";
+
+const CreateBlockModal = ({ active, setActive, newBlockNumber }) => {
+  const params = useParams();
+
+  const dispatch = useDispatch();
+  const { blocks, isFetching } = useSelector((state) => state.blocks);
+
+  const getLastBlockNumber = () => {
+    const maxBlockNumber = blocks.reduce(
+      (max, block) => (max > block.blockNumber ? max : block.blockNumber),
+      0
+    );
+    console.log(maxBlockNumber);
+    return maxBlockNumber + 1;
+  };
+
+  const createNewBlock = async (type, blockNumber) => {
+    if (!isFetching) {
+      const newBlockNumber = blockNumber ?? getLastBlockNumber();
+      console.log(newBlockNumber);
+      await dispatch(
+        createBlock(params.id, { type, blockNumber: newBlockNumber })
+      );
+      setActive(false);
+    }
+  };
+
+  return (
+    <Modal active={active} setActive={setActive}>
+      <button className={styles.closeButton} onClick={() => setActive(false)}>
+        Закрыть
+      </button>
+      <h2 className={styles.title}>Создать новый блок</h2>
+      <button
+        className={styles.button}
+        onClick={() => createNewBlock("ButtonBlock", newBlockNumber)}
+      >
+        Кнопка
+      </button>
+      <button
+        className={styles.button}
+        onClick={() => createNewBlock("ParagraphBlock", newBlockNumber)}
+      >
+        Параграф
+      </button>
+      <div className={styles.close}></div>
+    </Modal>
+  );
+};
+
+export default CreateBlockModal;
