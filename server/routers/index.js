fcf1b2172b3840e74d7497743df8efc8b7cc726d
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-04 00:33:42 +0300

    feat: add routes for styles

--- server/routers/index.js
+++ server/routers/index.js
@@ -5,7 +5,8 @@ const { body } = require("express-validator");
 const authMiddleware = require("../middlewares/auth-middleware");
 const checkProjectOwnerMiddleware = require("../middlewares/check-project-owner-middleware");
 const projectController = require("../controllers/project-controller");
-const blockController = require("../controllers/block-controller")
+const blockController = require("../controllers/block-controller");
+const stylesController = require("../controllers/styles-controller");
 
 //authorization
 router.post(
@@ -23,16 +24,39 @@ router.get("/users", authMiddleware, userController.getUsers);
 
 //projects
 router.post("/projects", authMiddleware, projectController.create);
-router.put("/projects/:id", authMiddleware,  projectController.update);
+router.put("/projects/:id", authMiddleware, projectController.update);
 router.get("/projects/:id", authMiddleware, projectController.get);
 router.get("/userProjects", authMiddleware, projectController.getUserProjects);
 router.delete("/projects/:id", authMiddleware, projectController.delete);
 
 //blocks
-router.post("/projects/:id/blocks", authMiddleware, checkProjectOwnerMiddleware, blockController.create);
-router.put("/projects/:id/blocks", authMiddleware, checkProjectOwnerMiddleware, blockController.update);
+router.post(
+  "/projects/:id/blocks",
+  authMiddleware,
+  checkProjectOwnerMiddleware,
+  blockController.create
+);
+router.put(
+  "/projects/:id/blocks",
+  authMiddleware,
+  checkProjectOwnerMiddleware,
+  blockController.update
+);
 router.get("/projects/:id/blocks", authMiddleware, blockController.getBlocks);
-router.get("/projects/:id/blocks/:blockId", authMiddleware, blockController.getBlock);
-router.delete("/projects/:id/blocks/:blockId", authMiddleware, checkProjectOwnerMiddleware, blockController.delete)
+router.get(
+  "/projects/:id/blocks/:blockId",
+  authMiddleware,
+  blockController.getBlock
+);
+router.delete(
+  "/projects/:id/blocks/:blockId",
+  authMiddleware,
+  checkProjectOwnerMiddleware,
+  blockController.delete
+);
+
+//styles
+router.get("/projects/:id/styles", authMiddleware, checkProjectOwnerMiddleware, stylesController.get);
+router.put("/projects/:id/styles", authMiddleware, checkProjectOwnerMiddleware, stylesController.update);
 
 module.exports = router;
