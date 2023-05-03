b72073e56bf0e82dd47deb73d1cf8ce17115ae10
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-04 00:33:15 +0300

    feat: add styles controller

---
+++ server/controllers/styles-controller.js
@@ -0,0 +1,29 @@
+const stylesService = require("../services/styles-service");
+
+class StylesController {
+  async get(req, res, next) {
+    try {
+      const projectId = req.params.id;
+
+      const styles = await stylesService.get(projectId);
+
+      res.json(styles);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async update(req, res, next) {
+    try {
+      const data = req.body;
+
+      const styles = await stylesService.update(data);
+
+      res.json(styles);
+    } catch (e) {
+      next(e);
+    }
+  }
+}
+
+module.exports = new StylesController();
