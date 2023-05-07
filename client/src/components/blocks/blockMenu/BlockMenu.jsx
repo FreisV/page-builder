6d91de834a81c978478e9816af57eab7e02de919
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-07 20:59:35 +0300

    feat: add modal window for changing styles

--- client/src/components/blocks/blockMenu/BlockMenu.jsx
+++ client/src/components/blocks/blockMenu/BlockMenu.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import styles from "./styles.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPen, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPen, faPlus, faTrash, faGear } from "@fortawesome/free-solid-svg-icons";
 import Button from "../../button/Button";
-import renderChange from "../helpers/renderChangeModal";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteBlock } from "../../../store/reducers/blocksReducer/actions";
 import { useParams } from "react-router-dom";
 import CreateBlockModal from "../modals/createBlockModal/CreateBlockModal";
+import renderChangeBlockModal from "../helpers/renderChangeBlockModal";
+import renderChangeStylesModal from "../helpers/renderChangeStylesModal";
 
 const BlockMenu = ({ id, children }) => {
-  const [changeActive, setChangeActive] = useState(false);
+  const [changeBlockActive, setChangeBlockActive] = useState(false);
+  const [changeStylesActive, setChangeStylesActive] = useState(false);
   const [createActive, setCreateActive] = useState(false);
 
   const params = useParams();
@@ -31,18 +33,28 @@ const BlockMenu = ({ id, children }) => {
   return (
     <div className={styles.menu}>
       <div className={styles.buttons}>
-        <Button onClick={() => setChangeActive(true)}>
-          <FontAwesomeIcon icon={faPen} />
-        </Button>
-        <Button onClick={() => setCreateActive(true)}>
-          <FontAwesomeIcon icon={faPlus} />
-        </Button>
+        <div>
+          <Button onClick={() => setChangeBlockActive(true)}>
+            <FontAwesomeIcon icon={faPen} />
+          </Button>
+          <Button onClick={() => setChangeStylesActive(true)}>
+          <FontAwesomeIcon icon={faGear} />
+          </Button>
+        </div>
+          <Button onClick={() => setCreateActive(true)}>
+            <FontAwesomeIcon icon={faPlus} />
+          </Button>
         <Button isRed={true} onClick={handleDelete}>
           <FontAwesomeIcon icon={faTrash} />
         </Button>
       </div>
       <div className={styles.content}>{children}</div>
-      {renderChange(changeActive, setChangeActive, block)}
+      {renderChangeBlockModal(changeBlockActive, setChangeBlockActive, block)}
+      {renderChangeStylesModal(
+        changeStylesActive,
+        setChangeStylesActive,
+        block
+      )}
       <CreateBlockModal
         active={createActive}
         setActive={setCreateActive}
