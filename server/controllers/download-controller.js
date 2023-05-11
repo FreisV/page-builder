99f91e2035b41b7ce7736dcdfdea80673b8c4f85
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-11 14:16:34 +0300

    feat: add download controller

---
+++ server/controllers/download-controller.js
@@ -0,0 +1,26 @@
+const downloadService = require("../services/download-service");
+
+class DownloadController {
+  async download(req, res, next) {
+    try {
+      const { user } = req;
+      const projectId = req.params.id;
+
+      const filePath = await downloadService.download(user.id, projectId);
+
+      res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+
+      res.download(filePath, (err) => {
+        if (err) {
+          console.error(`Ошибка загрузки файла: ${err}`);
+        } else {
+          console.log("Файл успешно загружен");
+        }
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+}
+
+module.exports = new DownloadController();
