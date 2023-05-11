3281ef40f809fe95b481836a7607d7cd482a255c
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-11 14:12:41 +0300

    feat: delete unused import

--- client/src/components/blocks/buttonBlock/ButtonBlock.jsx
+++ client/src/components/blocks/buttonBlock/ButtonBlock.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import BlockWrapper from "../blockWrapper/BlockWrapper";
-import { faW } from "@fortawesome/free-solid-svg-icons";
 
 const ButtonBlock = ({ id, ...otherProps }) => {
   const { blocks, blocksStyles } = useSelector((state) => state.blocks);
   const block = blocks.find((block) => block.id === id);
-  const blockStyles = blocksStyles.find(style => style.blockId === id);
-
+  const blockStyles = blocksStyles.find((style) => style.blockId === id);
 
   return (
     <BlockWrapper blockStyles={blockStyles}>
       <span>
-        <button style={{
-          color: blockStyles?.color,
-          backgroundColor: blockStyles?.buttonBackgroundColor,
-          border: blockStyles?.buttonBorderWidth + " solid " + blockStyles?.buttonBorderColor,
-          
-          padding:"20px 60px",
-          fontSize:"19px",
-          fontWeight:"600"
+        <button
+          style={{
+            color: blockStyles?.color,
+            backgroundColor: blockStyles?.buttonBackgroundColor,
+            border:
+              blockStyles?.buttonBorderWidth +
+              " solid " +
+              blockStyles?.buttonBorderColor,
 
-        }}
-        
-        >{block.text}</button>
+            padding: "20px 60px",
+            fontSize: "19px",
+            fontWeight: "600",
+          }}
+        >
+          {block.text}
+        </button>
       </span>
     </BlockWrapper>
   );
