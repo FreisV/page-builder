fed4bdfe52e0019e88b12225432bf943889e31b4
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-05-11 14:17:51 +0300

    feat: add download button

--- client/src/pages/project/Project.jsx
+++ client/src/pages/project/Project.jsx
@@ -4,10 +4,15 @@ import Header from "../../components/header/Header";
 import BlockList from "../../components/blockList/BlockList";
 import { useSelector } from "react-redux";
 import ErrorMessage from "../../components/errorMessage/ErrorMessage";
+import { useParams } from "react-router-dom";
+import Button from "../../components/button/Button";
+import DownloadService from "../../services/DownloadService";
 
 const Project = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [errorMessageActive, setErrorMessageActive] = useState(false);
+
+  const params = useParams();
 
   const { isFetching, error } = useSelector((state) => state.blocks);
 
@@ -23,11 +28,19 @@ const Project = () => {
     }
   }, [errorMessage]);
 
+  const download = async (e) => {
+    e.preventDefault();
+    await DownloadService.download(params.id);
+  };
+
   return (
     <>
       <Header />
       <main className={styles.wrapper}>
         <BlockList />
+        <div className={styles.button}>
+          <Button onClick={download}>Скачать</Button>
+        </div>
       </main>
 
       <ErrorMessage
