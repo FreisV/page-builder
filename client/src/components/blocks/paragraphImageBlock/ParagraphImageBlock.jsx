52ebbf5b5c7b3890a73a76509b790ca1db027aac
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-06-01 16:00:49 +0300

    feat: add paragraphImage block component

---
+++ client/src/components/blocks/paragraphImageBlock/ParagraphImageBlock.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import BlockWrapper from "../blockWrapper/BlockWrapper";
+import { API_URL } from "../../../http";
+
+const ParagraphImageBlock = ({ id, ...otherProps }) => {
+  const { blocks, blocksStyles } = useSelector((state) => state.blocks);
+  const block = blocks.find((block) => block.id === id);
+  const blockStyles = blocksStyles.find((style) => style.blockId === id);
+
+  return (
+    <BlockWrapper blockStyles={blockStyles}>
+      <div className="twoColumnsWrapper">
+        <p
+          className="paragraph"
+          style={{
+            color: blockStyles?.color,
+            textAlign: blockStyles?.textAlign,
+          }}
+        >
+          {block.text}
+        </p>
+      </div>
+      <div className="twoColumnsWrapper image-wrapper">
+        <img
+          className="image"
+          src={`${API_URL}/images/${block.filename}`}
+          alt=""
+          style={{
+            maxHeight: blockStyles?.maxHeight,
+          }}
+        />
+      </div>
+    </BlockWrapper>
+  );
+};
+
+export default ParagraphImageBlock;
