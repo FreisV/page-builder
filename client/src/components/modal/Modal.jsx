ef9711a0f4bb4109f33979b0670d5ba37d5e6c83
Vadim Zubovsky <vadimzubovski2525@gmail.com>
2023-06-04 16:08:42 +0300

    fix: prevent page scrolling when a modal window is open

--- client/src/components/modal/Modal.jsx
+++ client/src/components/modal/Modal.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./styles.module.css";
 
 const Modal = ({ active, setActive, children }) => {
+  useEffect(() => {
+    if (active) {
+      document.body.classList.add("modal-open");
+    } else {
+      document.body.classList.remove("modal-open");
+    }
+  }, [active]);
+
   return (
     <div
       className={active ? `${styles.window} ${styles.active}` : styles.window}
